5028da1de61381b5f8ef0b208d92fe92f956e65f
Carl GENNETAIS <gen.abon@gmail.com>
2016-03-22 20:38:12 +0100

    Réponses en couleur

--- quiz.js
+++ quiz.js
@@ -1,30 +1,30 @@
 // pos = position ds le quiz
 var pos = 0,
-    test, titre_test, question, choice, choices, chA, chB, chC, correct = 0;
+test, titre_test, question, choice, choices, chA, chB, chC, correct = 0;
 
 //TODO : gerer les reponses multiples ("AB", checkbox)
 //TODO : mettre réponses dans base de données
 var questions = [
-    ["What is 10 + 4?", "12", "14", "16", "B"],
-    ["What is 20 - 9?", "7", "13", "11", "42", "C"],
-    ["What is 7 x 3?", "21", "24", "25", "A"],
-    ["What is 8 / 2?", "10", "2", "4", "C"]
+["Quelle est la capitale de la France ?", "Lyon", "Paris", "Marseille", "B"],
+["Que font 20 - 9?", "7", "13", "11", "42", "C"],
+["Que font 7 x 3?", "21", "24", "25", "A"],
+["Que font 8 / 2?", "10", "2", "4", "C"]
 ];
 
 // fonction pour accéder à un élément par son ID
 function _(x) {
-    return document.getElementById(x);
+	return document.getElementById(x);
 }
 
 function renderQuestion() {
-    test = _("test");
-    if (pos >= questions.length) {
-        test.innerHTML = "<h2>You got " + correct + " of " + questions.length + " questions correct</h2>";
-        _("titre_test").innerHTML = "Test Completed";
-        pos = 0;
-        correct = 0;
-        return false;
-    }
+	test = _("test");
+	if (pos >= questions.length) {
+		test.innerHTML = "<h2>Votre score est de " + correct + " / " + questions.length + ".</h2>";
+		_("titre_test").innerHTML = "Test terminé";
+		pos = 0;
+		correct = 0;
+		return false;
+	}
     // Affichage du titre
     _("titre_test").innerHTML = "Question " + (pos + 1) + " sur " + questions.length;
     // Affichage de la question
@@ -32,23 +32,31 @@ function renderQuestion() {
     // test.innerHTML = "<p>Cette question a " + (questions[pos].length - 2) + " réponses possibles</p>";
     // affichage des choix de réponses
     for (var i = 1; i <= questions[pos].length - 2; i++) {
-        test.innerHTML += "<div class='radio'><label><input type='radio' name='choices' value='"
-        + String.fromCharCode(65 - 1 + i) + "'> "
-        + questions[pos][i] +
-        "</label><br>";
+    	test.innerHTML += "<div class='radio'><label id='labelRep"
+    	+ String.fromCharCode(65 - 1 + i)
+    	+ "'><input type='radio' name='choices' value='"
+    	+ String.fromCharCode(65 - 1 + i) + "'> "
+    	+ questions[pos][i] +
+    	"</label><br>";
     }
-    test.innerHTML += "<br><button onclick='checkAnswer()'>Submit Answer</button><br><br>";
+    test.innerHTML += "<br><button onclick='checkAnswer()'>Vérifier réponse</button><br><br>";
+    test.innerHTML += "<br><button onclick='nextQuestion()'>Question suivante</button><br><br>";
     test.innerHTML += "<div id='messageErreur' class='alert alert-danger' style='display:none' role='alert'>Veuillez sélectionner une réponse.</div>"
 }
 
+function nextQuestion() {
+	pos++;
+	renderQuestion();
+}
+
 function checkAnswer() {
-    choices = document.getElementsByName("choices");
-    choice = 0;
+	choices = document.getElementsByName("choices");
+	choice = 0;
     // on recupere la case cochée
     for (var i = 0; i < choices.length; i++) {
-        if (choices[i].checked) {
-            choice = choices[i].value;
-        }
+    	if (choices[i].checked) {
+    		choice = choices[i].value;
+    	}
     }
     // si pas de case choisie
     if (choice == 0) {
@@ -57,9 +65,12 @@ function checkAnswer() {
     }
     // si le choix correspond a la bonne reponse
     else if (choice == questions[pos][questions[pos].length-1]) {
-        correct++;
+    	correct++;
+    	_("labelRep"+choice).className += "rightAnswer";
     }
-    pos++;
-    renderQuestion();
+    else {
+    	_("labelRep"+choice).className += "wrongAnswer";
+    	_("labelRep"+questions[pos][questions[pos].length-1]).className += "correctAnswer";
+    }
 }
 window.addEventListener("load", renderQuestion, false);
