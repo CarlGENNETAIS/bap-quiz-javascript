ab5122c1dc183d72e7b6af4301d084c6b54703bc
Carl GENNETAIS <gen.abon@gmail.com>
2016-03-23 22:30:46 +0100

    nettoyage du code 2

--- quiz.js
+++ quiz.js
@@ -1,16 +1,18 @@
 // pos = position ds le quiz
 var pos = 0,
-    autoUpdateTimer = 0,
+    autoUpdateTimerID = 0,
     startDate = 0,
     secs = 0,
-    minutes = 0,
-    quiz = 0,
-    titre_quiz = 0,
-    question = 0,
+    mins = 0,
     choice = 0,
-    choices = 0,
-    correct = 0,
+    trueAnswer = 0,
+    score = 0,
     delai = 0; // delai en secondes avant la nouvelle question
+
+// Affiche les instructions au chargement de la page
+window.addEventListener("load", startPage, false);
+// Rafraichissement du timer toutes les secondes
+autoUpdateTimerID = window.setInterval(updateTimer, 1000);
 
 //TODO : mettre réponses dans base de données
 var questions = [
@@ -40,11 +42,11 @@ function startGame() {
 }
 
 function finishGame() {
-    $("#quiz").html("<h2>Votre score est de " + correct + " / " + questions.length + ".</h2>");
+    $("#quiz").html("<h2>Votre score est de " + score + " / " + questions.length + ".</h2>");
     $("#restartButton").show();
     $("#titre_quiz").html("Quiz terminé !");
     displayProgress();
-    clearInterval(autoUpdateTimer);
+    clearInterval(autoUpdateTimerID);
 }
 
 function renderQuestion() {
@@ -68,7 +70,6 @@ function renderQuestion() {
     displayProgress();
 }
 
-
 function displayProgress() {
 	// calcul de l'avancement en %
     var p = (pos / questions.length * 100);
@@ -81,65 +82,37 @@ function displayProgress() {
     }
 }
 
-function nextQuestion() {
-    pos++;
-    renderQuestion();
-}
-
-function getChoice() {
-    choices = document.getElementsByName("choices");
-    choice = 0;
-    for (var i = 0; i < choices.length; i++) {
-        if (choices[i].checked) {
-            choice = choices[i].value;
-        }
-    }
-    return choice;
-}
-
 // l'appel se fait dans le <input onchange="">
 function checkAnswer() {
-    // desactive les inputs apres la reponse
+    // desactive les inputs pendant la correction
     $("input").prop('disabled', true);
-    choices = document.getElementsByName("choices");
-    choice = getChoice();
-    // on recupere la case cochée
-    for (var i = 0; i < choices.length; i++) {
-        if (choices[i].checked) {
-            choice = choices[i].value;
-        }
-    }
+    // recupere la valeur de la case cochée (A,B,C ou D...)
+    choice = $('form input[type=radio]:checked').val();
+    // on recupere la correction dans le tableau
+    trueAnswer = questions[pos][questions[pos].length - 1];
     // si le choix correspond a la bonne reponse
-    if (choice == questions[pos][questions[pos].length - 1]) {
-        correct++;
-        _("labelRep" + choice).className += "rightAnswer";
+    if (choice == trueAnswer) {
+        $('input[value="'+choice+'"]').parent().addClass("rightAnswer");
+        score++;
     } else {
-        _("labelRep" + choice).className += "wrongAnswer";
-        var string = ".labelRep" + choice;
-        // $(string).wrap("<s></s>");
-        _("labelRep" + questions[pos][questions[pos].length - 1]).className += "rightAnswer";
+        $('input[value="'+choice+'"]').parent().addClass("wrongAnswer");
+        $('input[value="'+trueAnswer+'"]').parent().addClass("rightAnswer");
     }
-    // delai de 2s
-    setTimeout(function() { nextQuestion(); }, delai * 1000);
+    // delai puis lancement de la prochaine question en incrementant
+    setTimeout(function() { renderQuestion(pos++); }, delai * 1000);
 }
 
 function updateTimer() {
     // calcul secondes ecoulees depuis debut du test
     secs = (new Date() - startDate) / 1000;
-    minutes = secs / 60;
+    mins = secs / 60;
     secs = parseInt(secs % 60);
-    minutes = parseInt(minutes % 60);
+    mins = parseInt(mins % 60);
 
     // ajoute les zéros devant les chiffres
-    minutes = (minutes < 10 ? "0" : "") + minutes;
+    mins = (mins < 10 ? "0" : "") + mins;
     secs = (secs < 10 ? "0" : "") + secs;
 
-    $("#timer").text(minutes + ":" + secs);
+    $("#timer").text(mins + ":" + secs);
 }
 
-
-
-// Affiche les instructions au chargement de la page
-window.addEventListener("load", startPage, false);
-// Rafraichissement du timer toutes les secondes
-autoUpdateTimer = window.setInterval(updateTimer, 1000);
